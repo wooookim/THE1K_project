d431fb44c68928b92c8a1bb17e0ba6501f246763
wooookim <jhr01175@naver.com>
2024-02-23 16:07:39 +0900

    [add] mysql db에 저장

--- project7/apps/app.py
+++ project7/apps/app.py
@@ -12,6 +12,8 @@ import google.auth.transport.requests
 from flask_sqlalchemy import SQLAlchemy
 from flask import after_this_request
 
+import pymysql
+
 ##############
 
 app = Flask("Google Login App")
@@ -20,22 +22,39 @@ app.secret_key = "CodeSpecialist.com"
 
 ##############
 # SQLAlchemy 초기화
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "sqlite:///login_info.db"  # SQLite 데이터베이스를 사용하도록 설정
-)
-db = SQLAlchemy(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = (
+#     "sqlite:///login_info.db"  # SQLite 데이터베이스를 사용하도록 설정
+# )
+# db = SQLAlchemy(app)
 
 
 # 사용자 모델 정의
-class User(db.Model):
-    id = db.Column(db.String, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String)
+# class User(db.Model):
+#     id = db.Column(db.String, primary_key=True)
+#     name = db.Column(db.String)
+#     email = db.Column(db.String)
 
 
-# 데이터베이스 생성 및 초기화
-with app.app_context():
-    db.create_all()
+# # 데이터베이스 생성 및 초기화
+# with app.app_context():
+#     db.create_all()
+
+
+# MySql
+db = pymysql.connect(
+    host="35.202.242.82",
+    port=3306,
+    user="root",
+    passwd="the1k0219",
+    db="team05",
+    charset="utf8",
+)
+cursor = db.cursor()
+
+# class User(db.Model):
+#     id = db.Column(db.String, primary_key=True)
+#     name = db.Column(db.String)
+#     email = db.Column(db.String)
 ##############
 
 
@@ -125,17 +144,32 @@ def callback():
     session["google_id"] = id_info.get("sub")
     session["name"] = id_info.get("name")
 
-    ###
+    ### sqlite
     # 데이터베이스에 사용자 정보 저장
-    existing_user = User.query.filter_by(id=id_info.get("sub")).first()
+    # existing_user = User.query.filter_by(id=id_info.get("sub")).first()
+
+    # if not existing_user:
+    #     # 사용자가 데이터베이스에 없는 경우에만 추가
+    #     user = User(
+    #         id=id_info.get("sub"), name=id_info.get("name"), email=id_info.get("email")
+    #     )
+    #     db.session.add(user)
+    #     db.session.commit()
+
+    ### MySql
+    # 사용자가 이미 존재하는지 확인
+    select_sql = "SELECT * FROM USER WHERE USER_ID = %s"
+    cursor.execute(select_sql, (id_info.get("sub"),))
+    existing_user = cursor.fetchone()
 
     if not existing_user:
-        # 사용자가 데이터베이스에 없는 경우에만 추가
-        user = User(
-            id=id_info.get("sub"), name=id_info.get("name"), email=id_info.get("email")
-        )
-        db.session.add(user)
-        db.session.commit()
+        # 사용자가 존재하지 않으면 새로운 사용자 추가
+        insert_sql = "INSERT INTO USER(USER_ID, USER_PW, USER_NAME) VALUES (%s, %s, %s)"
+        userid = id_info.get("sub")
+        name = id_info.get("name")
+        email = id_info.get("email")
+        cursor.execute(insert_sql, (userid, name, email))
+        db.commit()
     ###
 
     return redirect("/google_protected_area")
@@ -144,7 +178,13 @@ def callback():
 @app.route("/google_protected_area")
 @login_is_required
 def google_protected_area():
-    user = User.query.filter_by(id=session["google_id"]).first()
+    ### sqlite
+    # user = User.query.filter_by(id=session["google_id"]).first()
+    ### mysql
+    select_sql = "SELECT * FROM USER WHERE USER_ID = %s"
+    cursor.execute(select_sql, (session["google_id"],))
+    user = cursor.fetchone()
+    print("User Information:", user)  # 확인을 위한 출력
     return render_template("logined_home.html", user=user)
 
 
